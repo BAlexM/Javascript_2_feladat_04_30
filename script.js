8656a90cc90185940efc950740c7b1cadb3983af
BAlexM <75310437+BAlexM@users.noreply.github.com>
2021-04-30 22:55:38 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,5 +3,5 @@ function fakt(f){let fac = 1
   for ( index = 1; index < f; index++) {fac = fac*(index+1)}return fac;}
 function hf(){document.write("<table class='style'>");
   for (index = 0; index < 21; index++) 
-  {if (index != 0) {document.write("<tr><td class='elso'>"+ index +"!</td><td class='masodik'>"+ fakt(index) +"</td></tr>")}
-  else{document.write("<tr><td class='elso'>"+ index +"!</td><td class='masodik'>"+ 1 +"</td></tr>")}}document.write("</table>");}hf();
+  {if (index != 0) {document.write("<tr><td class='elso'>"+ index +"!=</td><td class='masodik'>"+ fakt(index) +"</td></tr>")}
+  else{document.write("<tr><td class='elso'>"+ index +"!=</td><td class='masodik'>"+ 1 +"</td></tr>")}}document.write("</table>");}hf();
